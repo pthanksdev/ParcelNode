1b5a9b7002881757846872e54e3c2a9f84f7283e
Paul Thanksgiving <pthanksdev@gmail.com>
2026-09-06 18:43:33 -0600

    feat: add index file to export layout, shipments, admin, web3, settings, and common UI components

---
+++ frontend/components/index.ts
@@ -0,0 +1,33 @@
+// Layout Components
+export * from './layout/Navbar';
+export * from './layout/Footer';
+export * from './layout/MobileNavDrawer';
+export * from './layout/HeroSection';
+export * from './layout/RoiCalculatorSection';
+
+// Shipments Components
+export * from './shipments/ShipmentsTable';
+export * from './shipments/CreateShipmentModal';
+export * from './shipments/EditShipmentModal';
+export * from './shipments/DeleteShipmentModal';
+export * from './shipments/DashboardKpiCards';
+export * from './shipments/TrackingTimeline';
+export * from './shipments/AuditExporter';
+
+// Admin Governance Components
+export * from './admin/AdminOverviewTab';
+export * from './admin/CarrierWebhookSimulator';
+export * from './admin/SecurityAuditLogsTab';
+
+// Web3 Ledger Components
+export * from './web3/MerkleProofVerifier';
+export * from './web3/MerkleTreeVisualizer';
+export * from './web3/ChainStatusBadge';
+export * from './web3/ExplorerLink';
+
+// Settings & Integrations Components
+export * from './settings/ApiKeyManager';
+export * from './settings/CarrierConfigCard';
+
+// Common UI Components
+export * from './ui/ToastNotification';
